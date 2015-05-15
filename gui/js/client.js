fdbcf0f89f57cc170c94db195e1917ee6b26367b
Hiroyuki Baba <hiroyuki.baba.jp@gmail.com>
2015-05-15 16:19:47 +0000

    Implementing GUI

--- gui/js/client.js
+++ gui/js/client.js
@@ -1,4 +1,56 @@
+var app = angular.module("synthctrl",[]);
+var fileListCtrl = null;
+app.controller('FileList', function($scope){
+	$scope.files = ["hoge.json","fuga.json", "poko.json"];	
+	console.log($scope.files);
+	fileListCtrl = $scope;
+});
+
+var man = null;
+
 function run()
 {
 	alert("run");
 }
+
+function RPCManager(wsurl)
+{
+	this.srv_socket = io.conenct(wsurl);
+	this.RPCS = {};
+	this.RPCID = 0;	
+}
+RPCManager.prototype.RPC = function(evt, data, callback, options){
+	var me = this;
+	if(!('RPC' in me.SocketIOEvt)){
+		me.srv_socket.on('RPC', function(res){
+			me.RPCS[res.RPCID](res.data);
+			delete me.RPCS[res.RPCID];
+		});
+		me.SocketIOEvt['RPC'] = true;
+	}
+	me.RPCS[me.RPCID] = callback;
+	var req = {RPCID:me.RPCID, evt:evt, data:data};
+	me.srv_socket.emit('RPC', req);
+	me.RPCID++;
+};
+
+/**
+ * Broadcasting based RPC. (Utility)
+ */
+RPCManager.prototype.on = function(evt, callback, options)
+{
+	var me = this;
+	if(!(evt in me.SocketIOEvt)){
+		me.srv_socket.on(evt, callback);
+		me.SocketIOEvt[evt] = true;
+	}
+};
+
+$("document").ready(function(){
+	var man = new RPCManager("129.168.11.7:8081");
+
+	man.RPC('list', {}, function(res){
+		fileListCtrl.files = res;
+		fileListCtrl.$apply();		
+	});	
+});
--- gui/server.js
+++ gui/server.js
@@ -31,15 +31,6 @@ app.use(methodOverride());
 app.get('/', function(req, res){
   res.render('index');
 });
-
-app.get('/studio', function(req, res){
-  if(req.user == undefined){
-    res.render('studio', { user: '', login_menu:'<a href="/auth/facebook">login</a>'});
-  }else{
-    res.render('studio', { user: '<a href="#" onclick="onProfileDialog()">'+'Profile'+'</a>', login_menu:'<a href="/logout">logout</a>' });
-  }
-});
-
 function remove(array, val)
 {
 	for(var i = 0; i < array.length; ++i){
@@ -48,47 +39,6 @@ function remove(array, val)
 			break;
 		}
 	}
-}
-function newroom()
-{
-	return {member:[], info:null, delTimer:null};
-}
-
-function joinroom(socket, room)
-{
-	socket.join(room);
-	//if(!(room in rooms)){ console.log("ERROR"); return ; }
-	//rooms[room].member.push(socket.custom.peer_id);
-	mongo.db.collection('rooms').update({room_id:room},{$addToSet : {'member' : socket.custom.peer_id}});
-	clearDeleteTimer(room);
-	socket.custom.room = room;
-	socket.broadcast.to(socket.custom.room).emit('peer_joined',{peer:socket.custom.peer_id});
-}
-
-function clearDeleteTimer(room_id)
-{
-	/*
-	if(room_id in rooms){
-		if(rooms[room_id].delTimer){
-			console.log("Clear delete timer for room" + room_id);
-			clearTimeout(rooms[room_id].delTimer);
-		}
-		rooms[room_id].delTimer = null;
-	}*/
-}
-
-function setDeleteTimer(room_id)
-{
-/*
-	console.log("Set delete timer for room" + room_id);
-	clearDeleteTimer(room_id);
-	rooms[room_id].delTimer = setTimeout( function(){
-		if(room_id in rooms){
-			delete rooms[room_id];
-			console.log("Room deleted due to timeout : "+room_id);
-		}
-	}, 24*3600*1000); // Deleted after 24Hours
-*/
 }
 
 function leaveroom(socket)
@@ -128,79 +78,8 @@ function GUID(){
 }
 
 RPCBody = {
-	'join' : function(socket, data, res){
-		leaveroom(socket);
-		joinroom(socket, data.room);
-		//contentList[data.room] = contentList[data.room] || {};
-		setTimeout(function(){
-			mongo.db.collection('rooms').findOne({room_id:data.room},function(err,room){
-				cl = room.content_list || {};
-				socket.emit('content_list_update', cl);
-			});
-		},1); // To ensure order of RPC
-		console.log(rooms);
-		
-		mongo.db.collection('rooms').findOne({room_id : data.room}, function(err,room){
-			res.send({error:false, peers:room.member,info:room.info}); 
-		}); 
-		//return {error:false,peers:rooms[data.room].member, info:rooms[data.room].info};
-	},
-	'list_room' : function(socket, data, res){
-		mongo.db.collection('rooms').find({}).toArray(function(err, rooms){
-			var ret = [];
-			for(var i = 0; i < rooms.length; ++i){
-				var ri = rooms[i].info;
-				ri.id = rooms[i].room_id;
-				ret.push(ri);
-			}
-			res.send({error:false,rooms:ret});
-		});
-	},
-	'create_room' : function(socket, req, res){
-		{
-			var id = GUID();
-			//rooms[id] = newroom();
-			//rooms[id].info = req;
-			mongo.db.collection('rooms').update({room_id:id},{room_id:id, info:req, member:[],  content_list:{}},{upsert:true},function(err,ret){ console.log([err,ret]); });
-			//console.log(rooms);
-			setDeleteTimer(id);
-			res.send({error:false, id:id});
-		}
-	},
-	'publish' : function(socket, data, res){
-		  console.log("publish accepted : ");
-		  console.log(data);
-		  contentId = GUID(); // Generate content ID
-
-		  // Additional information added
-		  data.contentId = contentId;
-		  data.owner = socket.request.session.passport.user;
-		  
-		  var tmpVal = {};
-		  tmpVal["content_list."+contentId] = data;
-		  mongo.db.collection('rooms').update({room_id:socket.custom.room},{$set:tmpVal},function(err,res){
-		  	console.log([err,res]);
-		  });
-		  setTimeout(function(){
-			  mongo.db.collection('rooms').findOne({room_id:socket.custom.room},function(err,item){
-				console.log("content_list_update notofication sent");
-				console.log(item.content_list);
-			  	socket.emit('content_list_update', item.content_list);  // Because broadcast dont send to publisher itself.
-			  	socket.broadcast.to(socket.custom.room).emit('content_list_update', item.content_list); 
-			  });  
-	      	  },1); // To ensure order of RPC
-		  res.send({error:false, contentId:contentId});
-	},
-	'getprof' : function(socket, data, res){
-		console.log('getprof');
-		mongo.db.collection('users').findOne({user_id:data?data:socket.request.session.passport.user}, function(err,item){
-			if(err || (!item)){
-				res.send({nickname:'Player', profile:'I love music'});
-			}else
-			{
-				res.send(item.profile);
-			}
-		});
+	'list' : function(socket, data, res){
+		res.send({error:false, files:["hoge.json","baba.json","bubuka.json"]}); 
 	},
 	'setprof' : function(socket, data, res){
 		console.log('setprof : ' + JSON.stringify(data));
@@ -240,12 +119,6 @@ io.sockets.
   on('connection', function (socket) {
     console.log("Socket user id = " + JSON.stringify(socket.request.session.passport));
   socket.custom = {};
-  socket.emit('news', { hello: 'world' });
-  socket.on('hello', function (data) {
-	  console.log(data);
-	  socket.custom.peer_id = data.peer_id;
-	  socket.custom.room = null; 
-  });
   socket.on('RPC', function(req) {
 	  console.log('RPC call');
 	  console.log(req);
@@ -256,20 +129,7 @@ io.sockets.
 		 socket.emit('RPC',{RPCID:req.RPCID,error:true});
 	 }
   });
-  /*socket.on('join', function (req) {
-	  data = req.data;
-	  leaveroom(socket);
-	  joinroom(socket, data.room, req.RPCID);
-	  contentList[data.room] = contentList[data.room] || {};
-	  socket.emit('content_list_update', contentList[data.room]); 
-	  console.log(rooms);
-  });*/
   socket.on('disconnect', function (){
-	  leaveroom(socket);
-	  console.log(rooms);
-  });
-  socket.on('send', function (data) {
-	  socket.broadcast.to(socket.custom.room).emit('recv', {peer_id:socket.custom.peer_id, data:data});
   });
 });
 
